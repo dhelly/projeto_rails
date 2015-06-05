8047893f608e9e6661097be3bd2a6777c8139fc5
jaqueline <dhelly@gmail.com>
2015-06-05 00:08:22 +0000

    finalização do layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,22 +3,26 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
+    assert_select "title", "Home - Projeto RAD"
     assert_response :success
   end
 
   test "should get livro" do
     get :livro
     assert_response :success
+    assert_select "title", "Livro - Projeto RAD"
   end
 
   test "should get contato" do
     get :contato
     assert_response :success
+    assert_select "title", "Contato - Projeto RAD"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help - Projeto RAD"
   end
 
 end
